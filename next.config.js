60402ca63b6b68baba99ef466e698b0a0704dc90
jhozasa000 <jhozasa@hotmail.com>
2023-06-14 02:44:14 -0500

    change next.config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    distDir: '_static',
-    images: {
-      unoptimized: true
-    }
-  }
-  
-  module.exports = nextConfig 
-   
-   
-   
-   
-  
+    output: 'export'
+}
+
+module.exports = nextConfig
