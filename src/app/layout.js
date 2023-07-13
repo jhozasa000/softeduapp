90d42cd6cd218bae7d30416a0a13f225491f0faa
jhozasa000 <jhozasa@hotmail.com>
2023-07-12 21:19:10 -0500

    cambio a strictmode

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Roboto } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import { Context }  from './components/context/themecontext';
-
+import { StrictMode } from 'react';
 const roboto = Roboto({
   weight: ['300'],
   style: ['normal'],
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={'bg-light '+roboto.className}>
-          <Context>{children}</Context>
+         <StrictMode><Context>{children}</Context></StrictMode>
         </body>
     </html>
     </>
