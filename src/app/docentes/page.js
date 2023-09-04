4849d334c83216efcff8c510b988d7d8298f9a5c
jhozasa000 <jhozasa@hotmail.com>
2023-09-04 18:25:12 -0500

    modulo docente

--- src/app/docentes/page.js
+++ src/app/docentes/page.js
@@ -7,7 +7,7 @@ import { Alertas } from "../components/functions/helpers";
 import Menu from "../components/menu/Menu"
 import { useEffect, useRef, useState } from 'react';
 import Swal from 'sweetalert2';
-
+const folderImages = process.env.REACT_APP_FOLDER_IMAGE
 
 const metadata = {
     title: 'Docentes',
@@ -31,6 +31,7 @@ export default function Docentes(){
     const [fillteacher, setFillteacher] = useState('');
     const [loadteacher, setLoadteacher] = useState(true);
     const [btnpro, setBtnpro] = useState('Insertar');
+    const [btntea, setBtntea] = useState('Insertar');
 
     useEffect(() => {
         setLoadpro(false)
@@ -43,6 +44,7 @@ export default function Docentes(){
         loaddataTeacher()
     }, [loadteacher]);
 
+    //funciones profesion
     const insertBachelor = () =>{
         const inpNomPro = inputNomProfesion.current.value.trim()
         if(!inpNomPro){
@@ -54,7 +56,7 @@ export default function Docentes(){
         }
         Postdata('profesion/select',datos).then((ele) => {
             if(ele?.data?.length){
-                Alertas('Información','Ya existe la profesion')
+                Alertas('Información','Ya existe la profesión')
                 return false
             }else{
                 Postdata('profesion/insert',datos).then((res) => {
@@ -161,7 +163,7 @@ export default function Docentes(){
 
         Postdata('profesion/select',datos).then((ele) => {
             if(ele?.data?.length){
-                Alertas('Información','Ya existe el profesion con ese nombre')
+                Alertas('Información','Ya existe la profesión con ese nombre')
                 return false
             }else{
                 Putdata('profesion/edit',datos).then((res) => {
@@ -177,7 +179,7 @@ export default function Docentes(){
                         btnedit.onclick = function() { insertBachelor() }
                         div.appendChild(btnedit)
                         setBtnpro('Insertar')
-                        Alertas('Información', `Se actualizo la profesion en el sistema`)
+                        Alertas('Información', `Se actualizo la profesión en el sistema`)
                         inputNomProfesion.current.value = ''
                         setLoadpro(true)
                         
@@ -191,65 +193,241 @@ export default function Docentes(){
         Getdata('profesion/select').then((info)=>{
             setfillcarr( info.data.map(({id, name},x) =>{
                 return <option key={x+1} value={id}>{name}</option>
-
             }))
         })
     }
 
-    
 
-    const insertTeacher = () => {
-
-        if(!inp.current.value  || !inpcedula.current.value  || !inpPro.current.value  || !inpTel.current.value  || !inpDir.current.value ){
+    //funciones docentes
+    const insertTeacher = () =>{
+        if(!inp.current.value.trim()  || !inpcedula.current.value.trim()  || !inpPro.current.value.trim()  || !inpTel.current.value.trim()  || !inpDir.current.value.trim() ){
             Alertas('Información','Los campos no pueden estar vacíos')
             return false
         }
-       const id = inpcedula.current.value 
-       const validate = datasite.docentes.filter((ele) =>{
-            return ele.inpcedula == id
-        })
- 
-        if(validate.length){
-            Alertas('Información','Ya existe un docente registrado con la cédula ',inpcedula.current.value)
-            return false
+        const id = inpcedula.current.value 
+        const datos = {
+            numberid:id,
         }
 
+        Postdata('docentes/select',datos).then((ele) => {
+            if(ele?.data?.length){
+                Alertas('Información','Ya existe un docente registrado con la cédula ',inpcedula.current.value)
+                return false
+            }else{
+                const formdata = new FormData()
+                formdata.append('name',inp.current.value.trim());
+                formdata.append('numberid',inpcedula.current.value.trim());
+                formdata.append('profession',inpPro.current.value.trim());
+                formdata.append('telephone',inpTel.current.value.trim());
+                formdata.append('address',inpDir.current.value.trim());    
+                formdata.append(`files`, inpFile?.current?.files[0]??'');
+     
+  
+                Postdata('docentes/insert',formdata).then((res) => {
 
-        const formdata = new FormData()
-        formdata.append('inp',inp.current.value);
-        formdata.append('inpcedula',inpcedula.current.value);
-        formdata.append('inpPro',inpPro.current.value);
-        formdata.append('inptel',inpTel.current.value);
-        formdata.append('inpdir',inpDir.current.value);    
-        formdata.append(`filesteacher`, inpFile?.current?.files.length??0);
-        let cont = 0
-        for (let file of inpFile?.current?.files) {
-            formdata.append(`file-${cont++}`, file, file.name);
-        }
-        
-        const obj = new Object();
-        for (let pair of formdata.entries()) {
-            obj[pair[0]] = pair[1]
-        }
+                    if(res?.data?.error){
+                        Alertas('Información',ele?.data?.error)
+                    }
 
-        setDatasite((prevState) => ({...prevState,docentes:[
-                ...prevState.docentes,
-                obj,
-        ]}))
-        setLoadteacher(true)
+                    if(res?.data?.affectedRows > 0){
+                        Alertas('Información', `Se inserto el docente en el sistema`)
+                        inp.current.value = ''
+                        inpcedula.current.value = ''
+                        inpPro.current.value = ''
+                        inpTel.current.value = ''
+                        inpDir.current.value = ''
+                        inpFile.current.value = ''
+                        setLoadteacher(true)
+                    }
+                })
+            }
+          })
     }
 
     const loaddataTeacher = () =>{
-            setFillteacher( datasite.docentes.map(({inp,inpPro,inpcedula},x) =>{
+        Getdata('docentes/select').then((info)=>{
+            setFillteacher( info.data.map(({id, name, numberid, profession},x) =>{
+
+                const docentesdelete = (id) =>{
+                    const datos = {
+                        id:id
+                    }
+                    Swal.fire({
+                        title: `<strong>¿Desea eliminar: ${name}?</strong>`,
+                        showDenyButton: false,
+                        showCancelButton: true,
+                        confirmButtonText:'Eliminar',
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#3085d6',
+                        cancelButtonText:'Cancelar'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Putdata('docentes/delete',datos).then(res => {
+                                if(res?.data?.affectedRows > 0 ){
+                                    Alertas('Información',`Se eliminó docente ${name} del sistema`)
+                                    setLoadteacher(true)
+                                }
+                            })
+                        } 
+                    })
+                }
+
+
                 return <li key={x} className="list-group-item d-flex text-start text-wrap">
-                            <div className="ms-2 me-auto">
-                                <div className='text-primary fw-bold'>{inp} - {inpcedula}</div>
-                                    <i className="bi bi-arrow-right-circle ms-3">{inpPro}</i>
-                            </div>
-                            <span><i className="bi bi-trash fs-4 px-2 text-danger"></i></span>
-                            <span><i className="bi bi-pencil-square fs-4 px-2 text-success"></i></span>
-                        </li>
-        }))
+                        <div className="ms-2 me-auto">
+                            <div className='text-primary fw-bold'>{name} - {numberid}</div>
+                                <i className="bi bi-arrow-right-circle ms-3">{profession}</i>
+                        </div>
+                        <span><a onClick={() => docentesview(info.data[x])}><i className="bi bi-person-rolodex fs-4 px-2 text-primary"></i></a></span>
+                        <span><a onClick={() => docentesdelete(id)}><i className="bi bi-trash fs-4 px-2 text-danger"></i></a></span>
+                        <span><a onClick={() => docentesedit(info.data[x])}><i className="bi bi-pencil-square fs-4 px-2 text-success"></i></a></span>
+                    </li>
+            }))
+        })   
+    }
+
+    const docentesview = ({id,name,numberid,profession,telephone,address,files}) => {
+
+        const pdf = folderImages+files;
+        Alertas(`Información docente: `,
+            `
+            <table class="table text-start">
+                <tbody>
+                    <tr>
+                        <td>Nombre</td>
+                        <td>${name}</td>
+                    </tr>
+                    <tr>
+                        <td>Número identificación</td>
+                        <td>${numberid}</td>
+                    </tr>
+                    <tr>
+                        <td>Profesión</td>
+                        <td>${profession}</td>
+                    </tr>
+                    <tr>
+                        <td>Teléfono</td>
+                        <td>${telephone}</td>
+                    </tr>
+                    
+                    <tr>
+                        <td>Dirección</td>
+                        <td>${address}</td>
+                    </tr>
+                    <tr>
+                        <td>Archivo PDF</td>
+                        <td>${files.length?`<a href="${pdf}" download="docente-${numberid}.pdf">descargar</a>`:'0 archivos'}</td>
+                    </tr>
+                </tbody>
+            </table>
+           `
+            )
+    }
+
+    const docentesedit = ({id,name,numberid,idpro,telephone,address,files}) => {
+        setBtntea("Actualizar")
+        inp.current.value = name
+        inpcedula.current.value = numberid
+        inpcedula.current.setAttribute('readonly', true)
+        inpcedula.current.classList.add('bg-light')
+        inpPro.current.value = idpro
+        inpTel.current.value = telephone
+        inpDir.current.value = address
+
+        const div = document.getElementById("btntea")
+        div.innerHTML = ""
+
+        const btnedit = document.createElement('button')
+        btnedit.setAttribute('class', 'btn btn-primary mx-3 my-3')
+        btnedit.innerText = "Actualizar"
+        btnedit.id = 'btn_insert_sche'
+        btnedit.name = 'btn_insert_sche'
+        btnedit.onclick = function() { docentesupdate(id,files) }
+
+        const btncancel = document.createElement('button')
+        btncancel.setAttribute('class', 'btn btn-primary mx-3 my-3')
+        btncancel.innerText = "Cancelar"
+        btncancel.id = 'btn_cancel_sche'
+        btncancel.name = 'btn_cancel_sche'
+        btncancel.onclick = function() { docentescancel() }
+
+        div.appendChild(btnedit)
+        div.appendChild(btncancel)
+
+    }
+
+    const docentescancel = () => {
+        const div = document.getElementById("btntea")
+        div.innerHTML = ""
+
+        const btnedit = document.createElement('button')
+        btnedit.setAttribute('class', 'btn btn-primary my-3')
+        btnedit.innerText = "Insertar"
+        btnedit.id = 'btn_insert_sche'
+        btnedit.name = 'btn_insert_sche'
+        btnedit.onclick = function() { insertTeacher() }
+        div.appendChild(btnedit)
+        setBtntea('Insertar')
+        inp.current.value = ''
+        inpcedula.current.value = ''
+        inpcedula.current.removeAttribute('readonly')
+        inpcedula.current.classList.remove('bg-light')
+        inpPro.current.value = ''
+        inpTel.current.value = ''
+        inpDir.current.value = ''
+        inpFile.current.value = ''
+    }
+
+    const docentesupdate = (id,files) => {
+
+        const name = inp.current.value.trim()
+        const numberid =inpcedula.current.value.trim()
+        const profession = inpPro.current.value.trim()
+        const telephone = inpTel.current.value.trim()
+        const address = inpDir.current.value.trim()
+        const filesnew = inpFile?.current?.files[0]??''
+
+        if(!name || !numberid || !profession || !telephone || !address ){
+            Alertas('Información','Los campos no pueden estar vacíos')
+            return false
+        }
+
+        const formdata = new FormData()
+        formdata.append('id',id);
+        formdata.append('name',name);
+        formdata.append('numberid',numberid);
+        formdata.append('profession',profession);
+        formdata.append('telephone',telephone);
+        formdata.append('address',address);    
+        formdata.append(`files`, filesnew);
+        formdata.append(`filesbd`, files);
+     
+        Putdata('docentes/edit',formdata).then((res) => {
+            if(res?.data?.affectedRows > 0){
+                const div = document.getElementById("btntea")
+                div.innerHTML = ""
+
+                const btnedit = document.createElement('button')
+                btnedit.setAttribute('class', 'btn btn-primary my-3')
+                btnedit.innerText = "Insertar"
+                btnedit.id = 'btn_insert_sche'
+                btnedit.name = 'btn_insert_sche'
+                btnedit.onclick = function() { insertTeacher() }
+                div.appendChild(btnedit)
+                setBtntea('Insertar')
+                Alertas('Información', `Se actualizo docente en el sistema`)
+                inp.current.value = ''
+                inpcedula.current.value = ''
+                inpcedula.current.removeAttribute('readonly')
+                inpcedula.current.classList.remove('bg-light')
+                inpPro.current.value = ''
+                inpTel.current.value = ''
+                inpDir.current.value = ''
+                inpFile.current.value = ''
+                setLoadteacher(true)
+                
+            }
+        })
     }
 
 
@@ -281,7 +459,7 @@ export default function Docentes(){
                     </div>
                     <div className="col-sm-12 col-md-6 mb-2" >
                         <div className="card h-100">
-                            <h3 className="my-2 text-center">Insertar docente</h3>
+                            <h3 className="my-2 text-center">{btntea} docente</h3>
                             <div className="row align-items-center justify-content-center my-2">
                                 <label htmlFor="inpTea" className="col-5 col-form-label col-form-label-sm fs-4 fw-bold">Nombre docente</label>
                                 <div className="col-6">
@@ -317,14 +495,14 @@ export default function Docentes(){
                                 </div>
                             </div>
                             <div className="row align-items-center justify-content-center my-2">
-                                <span className="col-5 col-form-label col-form-label-sm fs-4 fw-bold">Archivos</span>
+                                <span className="col-5 col-form-label col-form-label-sm fs-4 fw-bold">Archivo</span>
                                 <div className="col-6">
-                                    <label htmlFor="inpFile" className="col-5 form-control form-control-sm border-ligth fs-4 fw-bold btn border-primary btn-sm">Subir archivos
-                                        <input type="file" multiple className="form-control" name='inpFile' id="inpFile" style={{display:'none'}} accept="image/*,.pdf"  ref={inpFile}/>
+                                    <label htmlFor="inpFile" className="col-5 form-control form-control-sm border-ligth fs-4 fw-bold btn border-primary btn-sm">Subir archivo PDF
+                                        <input type="file" className="form-control" name='inpFile' id="inpFile" style={{display:'none'}} accept=".pdf"  ref={inpFile}/>
                                     </label>
                                 </div>
                             </div>
-                            <div className="text-center"> 
+                            <div className="text-center" id="btntea"> 
                                 <button className='btn btn-primary my-3 ' onClick={insertTeacher}>Insertar</button>
                             </div>
 
