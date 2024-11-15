3af6784b221b1279b20c5a272d9d1821f5ce39e2
jhonnatan zapata saavedra <jhozasa@hotmail.com>
2024-11-14 19:47:39 -0500

    add loading

--- src/app/components/functions/Session.js
+++ src/app/components/functions/Session.js
@@ -1,21 +1,34 @@
 "use client";
-import { useEffect, useContext } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
-import { useGlobalContext , Reducer } from '../context/themecontext'; // Asegúrate de que esta importación sea correcta
+import { useGlobalContext } from '../context/themecontext';
+import { RingLoader } from 'react-spinners';
 
 export default function Session({ children }) {
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
   const pathname = usePathname();
   const { datasite } = useGlobalContext (); // Usa useContext para acceder al contexto
 
   useEffect(() => {
-    console.log('datasite.user ###########', datasite?.user);
-    console.log('usePathname -------------', pathname);
-
     if (!datasite?.user) {
       router.push('/');
+    } else {
+      setLoading(false); 
     }
-  }, [datasite, pathname]);
+  }, [datasite, pathname, router]);
 
+  // Mientras está en carga (o redirigiendo), no mostrar nada
+  if (loading) {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <RingLoader color="#0070f3" size={100} />
+      </div>
+    );
+  }
   return children;
 }
